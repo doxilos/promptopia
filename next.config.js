538161cdc1d37669478c964a307a4da136f0d186
Nurbaki Vural <nurbakivural@gmail.com>
2023-05-09 14:04:23 +0300

    fixed bugs

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ["1h3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"],
   },
   webpack(config) {
     config.experiments = {
